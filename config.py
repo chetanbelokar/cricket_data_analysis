dda79a86e97890a23910708446fedadacabc17e9
CHETAN BELOKAR <chetanbelokar@gmail.com>
2023-04-11 03:37:30 +0530

    port number for hosting project and file paths

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+
+
+PORT_NUMBER = 5010
+
+RESULT_FILE_PATH = os.path.join('data_collection','Result_df.csv')
+BATTING_FILE_PATH = os.path.join('data_collection','Batter_df.csv')
+BOWLING_FILE_PATH = os.path.join('data_collection','Bowler_df.csv')
+PLAYER_FILE_PATH = os.path.join('data_collection','Player_df.csv')
+
+
+CSV_FILE_PATH = os.path.join('data_processing','data_for_analysis.csv')
